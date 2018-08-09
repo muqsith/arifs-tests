291cfb357a2ca5077e13b0e354dcff53a0425c8d
Muqsith Irfan <abdulmuqsith.irfan@gmail.com>
2018-08-09 20:13:31 +0400

    Object traversal - done

--- src/js/array.js
+++ src/js/array.js
@@ -2,54 +2,53 @@
     The below array is both rendered and used as input for logic
  */
 
- const sample = [
+const sample = [
     [1, 5, 7, 9, 13, 31, 97, 42, 29, 17],
     [3, 9, 4, 6, 8, 5, 33, 29, 71, 83]
- ];
+];
 
- setTimeout(() => {
+setTimeout(() => {
     document.getElementById('input').innerText = JSON.stringify(sample, null, 2);
- }, 100);
- /** Edit above sample to vary the inputs */
+}, 100);
+/** Edit above sample to vary the inputs */
 
 
- /**
-  * Common is like you are doing AND operation between the arrays
-  * @param {*} input
-  */
- function findCommon(input) {
+/**
+ * Common is like you are doing AND operation between the arrays
+ * @param {*} input
+ */
+function findCommon(input) {
     const filterSet = sample[0];
     return sample.slice(1).reduce((acc, arrayElement) => {
         return acc.concat(arrayElement.filter((element) => {
             return (filterSet.indexOf(element) !== -1);
         }));
     }, []);
- }
+}
 
- /**
-  * Unique is like you are doing OR operation between the arrays
-  * Like if the element is present in first array 
-  * then don't pick even if exists in the further input array
-  * Ex: in our sample set the element 5 should not occur twice
-  * in our unique set
-  * @param {*} input 
-  */
- function findUnique(input) {
+/**
+ * Unique is like you are doing OR operation between the arrays
+ * Like if the element is present in first array 
+ * then don't pick even if exists in the further input array
+ * Ex: in our sample set the element 5 should not occur twice
+ * in our unique set
+ * @param {*} input 
+ */
+function findUnique(input) {
     return input.reduce((acc, arrayElement) => {
         return acc.concat(arrayElement.filter((element) => {
             return (acc.indexOf(element) === -1);
         }));
     }, []);
- }
+}
 
- const common = findCommon(sample);
- setTimeout(() => {
+const common = findCommon(sample);
+setTimeout(() => {
     document.getElementById('common-result').innerText = JSON.stringify(common, null, 2);
- }, 100);
+}, 100);
 
- const unique = findUnique(sample);
- setTimeout(() => {
+const unique = findUnique(sample);
+setTimeout(() => {
     document.getElementById('unique-result').innerText = JSON.stringify(unique, null, 2);
- }, 100);
+}, 100);
 
- 
--- src/js/object-traversal.js
+++ src/js/object-traversal.js
@@ -20,73 +20,130 @@ const sample1 = {
             m: '',
             n: '0402930',
             "nullstring": ""
-        }
+        },
+        o: [1, 0, 4]
     }
 };
- setTimeout(() => {
-    document.getElementById('input1').innerText = JSON.stringify(sample1, null, 2);
- }, 100);
+setTimeout(() => {
+    document.getElementById('input1').innerText = JSON.stringify(sample1, (key, val) => {
+        if (key && typeof val === 'undefined') {
+            return 'undefined';
+        } else {
+            return val;
+        }
+    }, 2);
+}, 100);
 
- // Sample#2 has circular reference
- let sampleData = {};
- const sample2 = {
-     a: "Hello",
-     b: {
-         c: undefined,
-         d: {
-             e: 0,
-             f: -1,
-             h: [],
-             i: {
-                 j: null,
-                 k: '',
-                 l: {
-                     m: "",
-                     n: sampleData,
-                     o: {
+// Sample#2 has circular reference
+let sampleData = {};
+const sample2 = {
+    a: "Hello",
+    b: {
+        b1: sampleData,
+        c: undefined,
+        d: {
+            e: 0,
+            f: -1,
+            a1: sampleData,
+            h: [],
+            i: {
+                j: null,
+                k: '',
+                l: {
+                    m: "",
+                    n: sampleData,
+                    o: {
                         p: 'yellow',
                         q: -4,
                         r: [-1]
-                     }
-                 }
-             }
-         }
-     }
- };
+                    }
+                }
+            },
+            s: sampleData
+        }
+    }
+};
 
- sampleData.data = sample2;
+sampleData.data = sample2;
 
- setTimeout(() => {
+setTimeout(() => {
     document.getElementById('input2').innerText = JSON.stringify(sample2, (key, val) => {
-        if (key === 'n') return '<<<circular ref>>>';
-        else return val;
+        if (key && val === sample2) {
+            return '<<<circular ref>>>'
+        } else if (typeof val === 'undefined') {
+            return 'undefined';
+        } else {
+            return val;
+        }
     }, 2);
- }, 100);
- console.log(sample2.b.d.i.l.n.data === sample2);
- /** Edit above sample to vary the inputs */
+}, 100);
+console.log('Sample2 has circular references: ', sample2.b.d.i.l.n.data === sample2);
+/** Edit above sample to vary the inputs */
 
- 
 
- // Answer # 1
 
- function printFalsyKeys(input) {
-     for (let k in input) {
-         console.log(k);
-     }
-     return !!input;
- }
- const result1 = printFalsyKeys(sample1);
- setTimeout(() => {
+// Answer # 1
+
+function objectTraversal(obj, fn) {
+    for (let key in obj) {
+        let val = obj[key];
+        if (typeof val !== 'object' || val === null) {
+            if (!val) {
+                fn(key, val);
+            }
+        } else if (!Array.isArray(val)) {
+            objectTraversal(val, fn);
+        }
+    }
+}
+
+function printFalsyKeys(input) {
+    let falseKeys = [];
+    objectTraversal(input, (falseKey) => {
+        falseKeys.push(falseKey);
+    });
+    return falseKeys;
+}
+const result1 = printFalsyKeys(sample1);
+setTimeout(() => {
     document.getElementById('result1').innerText = JSON.stringify(result1, null, 2);
- }, 100);
+}, 100);
 
 
+// Answer # 2
 
+const objectTraversalCR = function () { // objectTraversal with circular references
+    const seenValues = [];
+    const hasSeenValues = (o) => {
+        return seenValues.find((v) => {
+            return v === o;
+        });
+    };
+    return function traversalFn (obj, fn) {
+        for (let key in obj) {
+            let val = obj[key];
+            if (typeof val !== 'object' || val === null) {
+                if (!val) {
+                    fn(key, val);
+                }
+            } else if (!Array.isArray(val) && !hasSeenValues(val)) {
+                if (typeof val === 'object') {
+                    seenValues.push(val);
+                }
+                traversalFn(val, fn);
+            }
+        }
+    }
+}
 
- function printFalsyKeysWithCircularReference(input) {
-    return !!input;
- }
- const result2 = printFalsyKeysWithCircularReference(sample2);
- setTimeout(() => {
+function printFalsyKeysWithCircularReference(input) {
+    let falseKeys = [];
+    objectTraversalCR()(input, (falseKey) => {
+        falseKeys.push(falseKey);
+    });
+    return falseKeys;
+}
+const result2 = printFalsyKeysWithCircularReference(sample2);
+setTimeout(() => {
     document.getElementById('result2').innerText = JSON.stringify(result2, null, 2);
- }, 100);
+}, 100);

53b49108f64c6be9e44892e5f8e5790880b7c497
Muqsith Irfan <abdulmuqsith.irfan@gmail.com>
2018-08-10 16:41:17 +0400

    Tree - 3/4th done

--- src/js/tree.js
+++ src/js/tree.js
@@ -15,7 +15,7 @@ const getInput = (limit) => {
     return input;
 }
 
-const input = getInput(10);
+const input = getInput(4);
 setTimeout(() => {
     document.querySelector('#input').innerHTML = `[${input.join(', ')}]`;
 }, 10);
@@ -37,13 +37,13 @@ function Tree() {
 
     function addElementToTree(element, root) {
         if (!root) return;
-        if (root.value > element.value) {
+        if (root.value < element.value) {
             if (!root.right) {
                 root.right = element;
             } else {
                 addElementToTree(element, root.right);
             }
-        } else if (root.value < element.value) {
+        } else if (root.value > element.value) {
             if (!root.left) {
                 root.left = element;
             } else {
@@ -63,29 +63,36 @@ function Tree() {
 
     function removeElementFromTree(data, root) {
         if (!root) return ;
-        if (root.value === data) {
-            root = null;
-        } else if (root.value < data) {
-            removeElementFromTree(data, root.right);
-        } else if (root.value > data) {
-            removeElementFromTree(data, root.left);
+        if (root.right) {
+            if (root.right.value === data) {
+                root.right = null;
+            } else if (root.right.value < data) {
+                removeElementFromTree(data, root.right);
+            }
+        }
+        if (root.left) {
+            if (root.left.value === data) {
+                root.left = null;
+            } else if (root.left.value > data) {
+                removeElementFromTree(data, root.left);
+            }
         }
     }
 
     this.removeElement = function (data) {
-        removeElementFromTree(data, this.root);
-    }
-
-    this.findElement = function (data, searchMethod) {
-
+        if (this.root.value === data) {
+            this.root = null;
+        } else {
+            removeElementFromTree(data, this.root);
+        }        
     }
 
     this.depthFirstSearch = function (data) {
-
+        console.log('doing depth first search');
     }
 
     this.breadthFirstSearch = function (data) {
-        
+        console.log('doing breadth first search');
     }
 
     this.toString = function () {
@@ -100,9 +107,41 @@ for (let i of input) {
     tree.addElement(i);
 }
 
-setTimeout(() => {
-    document.querySelector('#output').innerHTML = tree.toString();
-}, 10);
+function renderTree() {
+    setTimeout(() => {
+        document.querySelector('#output').innerHTML = tree.toString();
+    }, 10);
+}
+renderTree();
+
+
+let selectedMethod = 'breadthfirst';
+
+window.addNumber = function () {
+    let val = parseInt(+document.querySelector('#inputfield').value);
+    tree.addElement(val);
+    renderTree();
+}
+
+window.removeNumber = function () {
+    let val = parseInt(+document.querySelector('#inputfield').value);
+    tree.removeElement(val);
+    renderTree();
+}
+
+window.searchNumber = function () {
+    let val = document.querySelector('#searchfield').value;
+    console.log('searching for ', val);
+    if (selectedMethod === 'breadthfirst') {
+        tree.breadthFirstSearch(val)
+    } else if (selectedMethod === 'depthfirst') {
+        tree.depthFirstSearch(val);
+    }
+}
+
+window.setSearchMethod = function (event) {
+    selectedMethod = event.currentTarget.value;
+}
 
 
 
